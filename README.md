53782e74415bb9f8fa8c844cfee3efad3b9aa246
Marcelo Siriaki <msiriaki@gmail.com>
2023-04-04 16:48:15 -0300

    Testando enviar para uma nova branch chamada notMain.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # my-first-repo
 My first repository
 Estou testando a questão das branches.
+Agora vou tentar enviar para a branch notMain.
--- helloWorld.js
+++ helloWorld.js
@@ -3,3 +3,4 @@ console.log("Hello people!");
 const idade = 33;
 console.log(idade);
 //Isso é só um teste para aprender a usar o Git e GitHub.
+//Agora vou tentar enviar para a branch notMain.
