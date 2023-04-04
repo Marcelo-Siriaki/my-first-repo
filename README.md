29e91f856775cbc7f8424672185a5666084741cc
Marcelo Siriaki <msiriaki@gmail.com>
2023-04-04 16:51:11 -0300

    No git push, acabei não especificando a branch. Agora vou especificar, e vamos ver se dá certo.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 My first repository
 Estou testando a questão das branches.
 Agora vou tentar enviar para a branch notMain.
+Não deu certo, pq eu dei apenas o git push, e não especifiquei para qual branch eu queria. Agora vou especificar.
--- helloWorld.js
+++ helloWorld.js
@@ -4,3 +4,4 @@ const idade = 33;
 console.log(idade);
 //Isso é só um teste para aprender a usar o Git e GitHub.
 //Agora vou tentar enviar para a branch notMain.
+//Não deu certo, pq eu dei apenas o git push, e não especifiquei para qual branch eu queria. Agora vou especificar.
