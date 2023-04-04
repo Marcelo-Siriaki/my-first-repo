9856926c3fb889557283ee019d8ae4ea63ac8505
Marcelo Siriaki <msiriaki@gmail.com>
2023-04-04 16:44:46 -0300

    Testando como envia do VS Code com diferentes branches

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # my-first-repo
 My first repository
+Estou testando a questão das branches.
--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,5 @@
 console.log("Hello, world!");
 console.log("Hello people!");
+const idade = 33;
+console.log(idade);
+//Isso é só um teste para aprender a usar o Git e GitHub.
